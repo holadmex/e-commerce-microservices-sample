6dee20dce1c4bb1717e49ab0ad2d7def68d3e4cc
holadmex <ojewumidimeji@gmail.com>
2024-05-17 15:44:49 +0100

    wow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,13 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.25.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
-    }
-
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
-    }
-  }
-
-  backend "s3" {
-    bucket = "terraf-test"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-  }
-
-  required_version = ">= 1.6.3"
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
 }
-##
-##
-##
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "gitops-eks"
+}
+
+#
